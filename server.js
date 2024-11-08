b39e68fc2045ca165c76d210322f1fd728ec6dd2
pongsiri-101 <uoyouf449@gmail.com>
2024-11-08 18:21:56 +0700

    prisma

--- server.js
+++ server.js
@@ -1,16 +1,44 @@
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser"); 
+//data bast used api show data 
+const {PrismaClient} = require("@prisma/client");
+const prisma = new PrismaClient();
+
+
+
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true}));
 
+//feth data in data base 
+app.get('/book/list',async(req,res)=>{
+    const data = await prisma.book.findMany()
+    res.send({data:data});
+});
+
+app.post('/book/create', async(req,res) => {
+    const data = req.body;
+    const result = await prisma.book.create({data:data});
+
+    res.send({result:result});
+});
+
+app.post('/book/createManual',async(req,res) =>{
+    const result = await prisma.book.create({
+        data:
+        {   isbn:"1002",
+            name:"PHP",
+            price:850
+        }
+    });
+    res.send({result:result});
+});
 
 app.get("/",(req,res)=>{
     res.send("jimmy");
 });
-
 
 app.get('/hello/:name',(req,res) => {
     res.send("hello " + req.params.name);
